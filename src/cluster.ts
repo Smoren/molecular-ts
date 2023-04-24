789c5aede6d860c4552724aa51efa547b9dec771
Smoren <ofigate@gmail.com>
2023-04-24 19:31:43 +0300

    upd

---
+++ src/cluster.ts
@@ -0,0 +1,28 @@
+import { AtomInterface } from './types/atomic';
+
+class Cluster implements Iterable<AtomInterface> {
+  id: string;
+  atoms: Set<AtomInterface>;
+  neighbours: Set<Cluster>;
+
+  add(atom: AtomInterface): void {
+    this.atoms.add(atom);
+  }
+
+  remove(atom: AtomInterface): void {
+    this.atoms.delete(atom);
+  }
+
+  [Symbol.iterator](): Iterable<AtomInterface> {
+    return this.atoms.values();
+  }
+}
+
+export class ClusterMap {
+  map: Map<string, Cluster> = new Map();
+  quantum: number;
+
+  constructor(quantum: number) {
+    this.quantum = quantum;
+  }
+}
