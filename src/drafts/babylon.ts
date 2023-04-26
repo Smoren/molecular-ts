f27a5846731ebb5ffa9b323f1673495949b0190f
Smoren <ofigate@gmail.com>
2023-04-26 18:30:34 +0300

    upd

--- src/drafts/babylon.ts
+++ src/drafts/babylon.ts
@@ -1,24 +1,43 @@
-import { ArcRotateCamera, Engine, Mesh, PointLight, Scene, StandardMaterial, Vector3 } from 'babylonjs';
+import {
+  ArcRotateCamera,
+  Engine,
+  Mesh,
+  PointLight,
+  Scene,
+  StandardMaterial,
+  Vector3,
+  MeshBuilder,
+} from 'babylonjs';
 import { NumericVector } from '../vector/types';
 
 export function testBabylon() {
   setTimeout(() => startBabylon(), 100);
 }
 
-function createOrigin(scene: Scene, radius: number, coords: NumericVector, color: NumericVector): Mesh {
+function createAtomMesh(scene: Scene, radius: number, coords: NumericVector, color: NumericVector): Mesh {
   // eslint-disable-next-line new-cap
-  const origin = Mesh.CreateSphere('origin', 10, radius*2, scene);
-  origin.position.x = coords[0];
-  origin.position.y = coords[1];
-  origin.position.z = coords[2];
+  const atomMesh = Mesh.CreateSphere('origin', 10, radius*2, scene);
+  atomMesh.position.x = coords[0];
+  atomMesh.position.y = coords[1];
+  atomMesh.position.z = coords[2];
 
   const material = new StandardMaterial('material', scene);
   material.diffuseColor.r = color[0];
   material.diffuseColor.g = color[1];
   material.diffuseColor.b = color[2];
-  origin.material = material;
+  atomMesh.material = material;
 
-  return origin;
+  return atomMesh;
+}
+
+function createLinkMesh(scene: Scene, lhs: Mesh, rhs: Mesh, instance?: Mesh): Mesh {
+  // eslint-disable-next-line new-cap
+  return MeshBuilder.CreateTube('tube', {
+    path: [lhs.position, rhs.position],
+    updatable: true,
+    radius: 3,
+    instance,
+  }, scene);
 }
 
 function createLight(scene: Scene, coords: NumericVector, intensity: number): PointLight {
@@ -46,23 +65,32 @@ function startBabylon() {
   createLight(scene, [-200, -630, -598], 0.003);
 
   // eslint-disable-next-line new-cap
-  const origin = createOrigin(scene, 50, [0, 0, 0], [255, 0, 0]);
-  const origin1 = createOrigin(scene, 30, [100, 100, 100], [0, 0, 255]);
+  const atomMesh1 = createAtomMesh(scene, 50, [0, 0, 0], [255, 0, 0]);
+  const atomMesh2 = createAtomMesh(scene, 30, [100, 100, 100], [0, 0, 255]);
+  const linkMesh = createLinkMesh(scene, atomMesh1, atomMesh2);
 
-  let alpha = 0;
   let alpha1 = 0;
+  let alpha2 = 0;
 
   const tick = () => {
-    origin.position.x += 2 * Math.cos(alpha);
-    origin.position.z += 1 * Math.sin(alpha);
-    alpha += 0.02;
-
-    origin1.position.y += 2 * Math.cos(alpha1);
-    origin1.position.z += 2 * Math.sin(alpha1);
+    atomMesh1.position.x += 2 * Math.cos(alpha1);
+    atomMesh1.position.z += 1 * Math.sin(alpha1);
     alpha1 += 0.02;
+
+    atomMesh2.position.y += 2 * Math.cos(alpha2);
+    atomMesh2.position.z += 2 * Math.sin(alpha2);
+    alpha2 += 0.02;
+
+    if (!linkMesh.isDisposed()) {
+      createLinkMesh(scene, atomMesh1, atomMesh2, linkMesh);
+    }
 
     setTimeout(tick, 10);
   };
+
+  setTimeout(() => {
+    linkMesh.dispose();
+  }, 3000);
 
   tick();
 
