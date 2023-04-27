840de64bb9d527f9aa47a535acb290a9b2cd58ff
Smoren <ofigate@gmail.com>
2023-04-27 10:08:48 +0300

    upd

--- src/app/3d.ts
+++ src/app/3d.ts
@@ -4,6 +4,7 @@ import {
   WorldConfig,
 } from '../types/config';
 import { createBaseTypesConfig } from '../config/types';
+// import { createRandomTypesConfig } from '../config/types';
 import { createBaseWorldConfig } from '../config/world';
 import { create3dBaseInitialConfig } from '../config/initial';
 // import { create3Butterfly } from '../config/atoms';
@@ -13,6 +14,12 @@ import { create3dDrawer } from '../drawer/3d';
 
 const WORLD_CONFIG: WorldConfig = createBaseWorldConfig();
 const TYPES_CONFIG: TypesConfig = createBaseTypesConfig();
+// const TYPES_CONFIG: TypesConfig = createRandomTypesConfig({
+//   TYPES_COUNT: 7,
+//   GRAVITY_BOUNDS: [-1, 1],
+//   LINK_BOUNDS: [1, 5],
+//   LINK_TYPE_BOUNDS: [0, 4],
+// });
 const INITIAL_CONFIG: InitialConfig = create3dBaseInitialConfig();
 
 export function create3dSimulation() {
