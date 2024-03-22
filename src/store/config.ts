6e0c11509b1b005ad9379e8c55d58e769272a91d
Smoren <ofigate@gmail.com>
2024-03-22 13:36:11 +0300

    store updated

--- src/store/config.ts
+++ src/store/config.ts
@@ -1,4 +1,4 @@
-import { type Ref, ref } from "vue";
+import { type Ref, ref, watch } from "vue";
 import { defineStore } from "pinia";
 import type { InitialConfig, TypesConfig, WorldConfig } from "@/lib/types/config";
 import { createBaseWorldConfig } from "@/lib/config/world";
@@ -6,13 +6,47 @@ import { createBaseTypesConfig } from "@/lib/config/types";
 import { create3dBaseInitialConfig } from "@/lib/config/initial";
 
 export const useConfigStore = defineStore("config", () => {
-  const worldConfig: Ref<WorldConfig> = ref(createBaseWorldConfig());
-  const typesConfig: Ref<TypesConfig> = ref(createBaseTypesConfig());
-  const initialConfig: Ref<InitialConfig> = ref(create3dBaseInitialConfig());
+  const worldConfigRaw: WorldConfig = createBaseWorldConfig();
+  const typesConfigRaw: TypesConfig = createBaseTypesConfig();
+  const initialConfigRaw: InitialConfig = create3dBaseInitialConfig();
+
+  const worldConfig: Ref<WorldConfig> = ref(worldConfigRaw);
+  const typesConfig: Ref<TypesConfig> = ref(typesConfigRaw);
+  const initialConfig: Ref<InitialConfig> = ref(initialConfigRaw);
+
+  const getConfigValues = () => {
+    return {
+      worldConfig: worldConfigRaw,
+      typesConfig: typesConfigRaw,
+      initialConfig: initialConfigRaw,
+    }
+  }
+
+  watch(worldConfigRaw, (newConfig) => {
+    for (const i in newConfig) {
+      // @ts-ignore
+      worldConfigRaw[i] = newConfig[i];
+    }
+  }, { deep: true });
+
+  watch(typesConfig, (newConfig) => {
+    for (const i in newConfig) {
+      // @ts-ignore
+      typesConfigRaw[i] = newConfig[i];
+    }
+  }, { deep: true });
+
+  watch(initialConfig, (newConfig) => {
+    for (const i in newConfig) {
+      // @ts-ignore
+      initialConfigRaw[i] = newConfig[i];
+    }
+  }, { deep: true });
 
   return {
     worldConfig,
     typesConfig,
     initialConfig,
-  };
+    getConfigValues,
+  }
 });
