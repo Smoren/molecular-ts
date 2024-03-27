740aa443e505ba62afc2c4014290cdad6befd800
Smoren <ofigate@gmail.com>
2024-03-27 10:03:05 +0300

    symmetric as param

--- src/components/config-editor/components/config-matrix.vue
+++ src/components/config-editor/components/config-matrix.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 
 import { getColorString } from '@/components/config-editor/helpers/utils';
+import { ref, watch } from "vue";
 
 const props = withDefaults(defineProps<{
   colors: [number, number, number][];
@@ -9,16 +10,29 @@ const props = withDefaults(defineProps<{
   min?: number;
   max?: number;
   step?: number;
-  symmetric?: boolean;
 }>(), {
   step: 1,
 });
 
+const symmetric = ref(false);
+
 const onChangeValue = (i: number, j: number) => {
-  if (props.symmetric && i !== j) {
+  if (symmetric && i !== j) {
     props.values[j][i] = props.values[i][j];
   }
 };
+
+watch(symmetric, (value: boolean) => {
+  if (!value) {
+    return;
+  }
+
+  for (let i = 1; i < props.values.length; ++i) {
+    for (let j = 0; j < i; ++j) {
+      props.values[i][j] = props.values[j][i];
+    }
+  }
+});
 
 </script>
 
@@ -27,7 +41,9 @@ const onChangeValue = (i: number, j: number) => {
     <div style="text-align: center"> {{ name }} </div>
     <table>
       <tr>
-        <td></td>
+        <td>
+          <input type="checkbox" v-model="symmetric" title="Symmetric" />
+        </td>
         <td v-for="color in colors" :style="{ backgroundColor: getColorString(color) }">
           &nbsp;
         </td>
