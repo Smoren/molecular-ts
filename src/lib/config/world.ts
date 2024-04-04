4f07f11c8d5a2f77f8a3ae0ab58a1a0a97508278
Smoren <ofigate@gmail.com>
2024-04-04 12:40:35 +0300

    v2 physic model is default now.

--- src/lib/config/world.ts
+++ src/lib/config/world.ts
@@ -3,7 +3,7 @@ import type { NumericVector } from '../vector/types';
 
 export function createBaseWorldConfig(): WorldConfig {
   return {
-    PHYSIC_MODEL: 'v1',
+    PHYSIC_MODEL: 'v2',
     ATOM_RADIUS: 5,
     MAX_INTERACTION_RADIUS: 100,
     MAX_LINK_RADIUS: 60,
