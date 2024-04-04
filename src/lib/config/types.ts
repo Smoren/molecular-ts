d034a9fe4a4b407f4a2ac15738a7751f38fca5ef
Smoren <ofigate@gmail.com>
2024-04-04 12:46:51 +0300

    Randomize radius bounds updated.

--- src/lib/config/types.ts
+++ src/lib/config/types.ts
@@ -167,7 +167,7 @@ export function createRandomTypesConfig({
 export function createDefaultRandomTypesConfig(typesCount: number): RandomTypesConfig {
   return {
     TYPES_COUNT: typesCount,
-    RADIUS_BOUNDS: [0.5, 1.5, 1, 0.1],
+    RADIUS_BOUNDS: [0.75, 1.25, 1, 0.1],
     GRAVITY_BOUNDS: [-2, 1, -1, 0.1],
     LINK_GRAVITY_BOUNDS: [-5, 1, -1, 0.1],
     LINK_BOUNDS: [1, 3, 2],
