c22bfe21d9b008840ea1a9accce0f2b06e04fe4d
Smoren <ofigate@gmail.com>
2024-04-10 10:47:43 +0300

    Chart component is ready for tests.

---
+++ src/components/config-editor/components/widgets/chart.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+
+import { onMounted, onUnmounted, ref, type Ref } from 'vue';
+import type { ITimeSeriesPresentationOptions } from 'smoothie';
+import { SmoothieChart, TimeSeries } from 'smoothie';
+
+type TimeSeriesConfig = {
+  name: string;
+  options: ITimeSeriesPresentationOptions;
+  getter: () => number;
+}
+
+const props = defineProps<{
+  id: string;
+  name: string;
+  config: Record<string, TimeSeriesConfig>;
+  period: number;
+  width: number;
+  height: number;
+}>();
+
+const chart = new SmoothieChart({
+  grid: {
+    strokeStyle: 'rgb(80, 80, 80)',
+    fillStyle: 'rgb(0, 0, 0)',
+    lineWidth: 1,
+    millisPerLine: 1250,
+    verticalSections: 6,
+  },
+  labels: {fillStyle: 'rgb(255, 255, 255)'}
+});
+
+const interval: Ref<number | undefined> = ref();
+const timeSeries: Ref<Record<string, TimeSeries>> = ref({});
+
+const init = (): void => {
+  chart.streamTo(document.getElementById('chart') as HTMLCanvasElement);
+
+  for (const key in props.config) {
+    const config = props.config[key];
+    const series = new TimeSeries();
+
+    timeSeries.value[key as string] = series;
+    chart.addTimeSeries(series, config.options);
+  }
+
+  interval.value = setInterval(() => {
+    const now = Date.now();
+    for (const key in props.config) {
+      const config = props.config[key];
+      const summary = config.getter();
+      const series = timeSeries.value[key] as TimeSeries;
+      series.append(now, summary);
+    }
+  }, props.period);
+
+  chart.start();
+};
+
+const dispose = (): void => {
+  chart.stop();
+
+  for (const key in timeSeries.value) {
+    chart.removeTimeSeries(timeSeries.value[key]);
+    delete timeSeries.value[key];
+  }
+
+  clearInterval(interval.value);
+};
+
+onMounted(() => {
+  init();
+  console.log(`chart '${props.id}' mounted`);
+})
+
+onUnmounted(() => {
+  dispose();
+  console.log(`chart '${props.id}' unmounted`);
+})
+
+</script>
+
+<template>
+  <b>{{ name }}</b>
+  <canvas :id="`chart-${id}`" :width="width" :height="height"></canvas>
+</template>
+
+<style scoped lang="scss">
+
+@import "../../assets/config-editor";
+
+#chart {
+  width: 100%;
+}
+
+</style>
