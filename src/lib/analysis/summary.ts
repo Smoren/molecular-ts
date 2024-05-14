076ff8af64da49a226534b36600ac593f8608e51
Smoren <ofigate@gmail.com>
2024-05-14 15:49:44 +0300

    Fix for summary.

--- src/lib/analysis/summary.ts
+++ src/lib/analysis/summary.ts
@@ -209,13 +209,13 @@ export class SummaryManager implements SummaryManagerInterface {
 
     this.stepManager.buffer.LINKS_CREATED_MEAN = [...this.stepManager.buffer.LINKS_CREATED];
     this.stepManager.buffer.LINKS_DELETED_MEAN = [...this.stepManager.buffer.LINKS_DELETED];
-    this.finishMean('LINKS_CREATED_MEAN', this.stepManager.buffer.ATOMS_COUNT);
-    this.finishMean('LINKS_DELETED_MEAN', this.stepManager.buffer.ATOMS_COUNT);
+    this.finishMean('LINKS_CREATED_MEAN', this.stepManager.buffer.LINKS_COUNT);
+    this.finishMean('LINKS_DELETED_MEAN', this.stepManager.buffer.LINKS_COUNT);
 
     this.stepManager.buffer.LINKS_TYPE_CREATED_MEAN = [...this.stepManager.buffer.LINKS_TYPE_CREATED];
     this.stepManager.buffer.LINKS_TYPE_DELETED_MEAN = [...this.stepManager.buffer.LINKS_TYPE_DELETED];
-    this.finishArrayMean('LINKS_TYPE_CREATED_MEAN', this.stepManager.buffer.ATOMS_TYPE_COUNT);
-    this.finishArrayMean('LINKS_TYPE_DELETED_MEAN', this.stepManager.buffer.ATOMS_TYPE_COUNT);
+    this.finishArrayMean('LINKS_TYPE_CREATED_MEAN', this.stepManager.buffer.ATOMS_TYPE_LINKS_COUNT);
+    this.finishArrayMean('LINKS_TYPE_DELETED_MEAN', this.stepManager.buffer.ATOMS_TYPE_LINKS_COUNT);
 
     this.updateFpsMetrics();
     this.stepManager.save();
