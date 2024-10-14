6a29cb68e3bb4b2d07196ece8728f1792d7c4c0a
Smoren <ofigate@gmail.com>
2024-10-14 18:20:57 +0300

    upd

--- src/web/utils/backward.ts
+++ src/web/utils/backward.ts
@@ -9,6 +9,7 @@ export function convertTypesConfigForBackwardCompatibility(inputConfig: TypesCon
   const config = fullCopyObject(inputConfig);
 
   renameKey(config, 'LINK_FACTOR_DISTANCE_EXTENDED', 'LINK_FACTOR_DISTANCE');
+  deleteKey(config, 'LINK_FACTOR_DISTANCE_USE_EXTENDED');
 
   return config;
 }
@@ -21,6 +22,13 @@ function renameKey<T extends Record<string, unknown>>(input: T, oldKey: string, 
   if (input[oldKey] !== undefined) {
     input[newKey as keyof T] = input[oldKey] as T[keyof T];
     delete input[oldKey];
+  }
+  return input;
+}
+
+function deleteKey<T extends Record<string, unknown>>(input: T, key: string): T {
+  if (input[key] !== undefined) {
+    delete input[key];
   }
   return input;
 }
