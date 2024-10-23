c2bd214f950d2cf7ebcd8d358a4bee3bfe705098
Smoren <ofigate@gmail.com>
2024-10-23 19:28:43 +0300

    upd

--- src/lib/analysis/symmetry.ts
+++ src/lib/analysis/symmetry.ts
@@ -19,7 +19,8 @@ function getAverageRadius(graph: GraphInterface, centroid: NumericVector): numbe
 }
 
 function getAzimuth(vertex: Vertex, centroid: NumericVector): number {
-  // atan2(v.y - M.y, v.x - M.x) % PI
+  // Определим функцию azimuth(v) = atan2(v.y - M.y, v.x - M.x) % PI.
+  // (Смысл взятия по модулю — спроецировать все точки в верхнюю полуплоскость относительно точки M.)
   return Math.atan2(
     ...createVector(vertex.position).sub(centroid).reverse() as [number, number]
   ) % Math.PI;
@@ -32,16 +33,53 @@ function getSortedVertexes(vertexes: Vertex[], centroid: NumericVector): Vertex[
   );
 }
 
+function distanceToLine(point: NumericVector, k: number, b: number): number {
+  const [x0, y0] = point;
+  const numerator = Math.abs(k * x0 - y0 + b);
+  const denominator = Math.sqrt(k * k + 1);
+  return numerator / denominator;
+}
+
+function checkSlopeCoefficient(vertexes: Vertex[], k: number, b: number, radius: number, magic: number): boolean {
+  // Перебираем все вершины:
+  for (const vertex of vertexes) {
+    const dist = distanceToLine(vertex.position, k, b);
+
+    // Если расстояние от точки (вершина) до прямой (ось-кандидат с коэффициентом k) не превышает R * MAGIC,
+    // где MAGIC — некая экспериментально подобранная константа, считаем, что эта вершина лежит на оси симметрии,
+    // и игнорируем ее.
+    if (dist <= radius * magic) {
+      continue;
+    }
+
+  }
+}
+
 // TODO rename function
-function iterateSortedVertexes(sortedVertexes: Vertex[], centroid: NumericVector): void {
+function iterateSortedVertexes(graph: GraphInterface): void {
+  // Найдем точку M — центр масс графа. Если граф действительно симметричен, ось симметрии будет проходить через M —
+  // нам остаётся найти ее угловой коэффициент.
+  const centroid = getCentroid(graph);
+
+  // Вычислим R = avg([dist(v, M) for v in vertexes]), где avg(arr) — среднее арифметическое.
+  const radius = getAverageRadius(graph);
+
+  // Вычислим reordered = sorted(vertexes, key=azimuth).
+  const sortedVertexes = getSortedVertexes(graph.vertexes, centroid);
+
+  // Переберем соседние пары вершин (a = reordered[i]; b = reordered[(i + 1) % N])
   const iter = single.limit(infinite.cycle(sortedVertexes), sortedVertexes.length+1);
 
   single.map(single.pairwise(iter), ([lhs, rhs]) => {
     // Проверим ось симметрии с угловым коэффициентом tan(azimuth(a)) — пытаемся провести прямую через точку a.
-    const slopeCoefficient1 = Math.tan(getAzimuth(lhs, centroid));
+    const k1 = Math.tan(getAzimuth(lhs, centroid));
+    const p1 = lhs.position;
+    const b1 = p1[1] - k1 * p1[0]; // y = kx + b => b = y - kx
 
     // Проверим ось симметрии с угловым коэффициентом tan((azimuth(a) + azimuth(b)) * 0.5) — пытаемся провести
     // посередине между a и b (собственно, ради этого и затевалась сортировка по азимутам).
-    const slopeCoefficient2 = Math.tan(getAzimuth(lhs, centroid) + getAzimuth(rhs, centroid)) * 0.5;
+    const k2 = Math.tan(getAzimuth(lhs, centroid) + getAzimuth(rhs, centroid)) * 0.5;
+    const p2 = createVector(lhs.position).add(rhs.position).div(2);
+    const b2 = p2[1] - k2 * p2[0]; // y = kx + b => b = y - kx
   });
 }
