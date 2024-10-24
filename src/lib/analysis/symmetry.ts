580ae29678c9d528b523d63d70e82bd86b4dea85
Smoren <ofigate@gmail.com>
2024-10-24 23:06:39 +0300

    scoreBilateralSymmetry function implemented, not tested.

---
+++ src/lib/analysis/symmetry.ts
@@ -0,0 +1,86 @@
+import { infinite, single } from "itertools-ts";
+import type { LineCoefficients } from "../math/types";
+import type { GraphInterface } from "../graph/types";
+import { createVector, toVector } from "../math";
+import {
+  calcDistanceBetweenGraphsByTypesCombined,
+  getGraphAverageRadius,
+  getGraphCentroid,
+  getVertexAzimuth,
+  getVertexesSortedByAzimuth,
+  splitGraphByLine,
+} from "../graph/utils";
+import { getLineByPoints } from "../math/geometry";
+
+export function scoreSymmetryAxis(
+  graph: GraphInterface,
+  line: LineCoefficients,
+  radius: number,
+  magic: number,
+): number {
+  // Если расстояние от точки (вершина) до прямой (ось-кандидат с коэффициентом k) не превышает R * MAGIC,
+  // где MAGIC — некая экспериментально подобранная константа, считаем, что эта вершина лежит на оси симметрии
+  // minDistance = R * MAGIC
+  const [lhsGraph, rhsGraph] = splitGraphByLine(graph, line, radius*magic);
+
+  return -calcDistanceBetweenGraphsByTypesCombined(lhsGraph, rhsGraph);
+}
+
+export function scoreBilateralSymmetry(graph: GraphInterface): [number, LineCoefficients] {
+  // Найдем точку M — центр масс графа. Если граф действительно симметричен, ось симметрии будет проходить через M —
+  // нам остаётся найти ее угловой коэффициент.
+  const centroid = getGraphCentroid(graph);
+
+  // Вычислим R = avg([dist(v, M) for v in vertexes]), где avg(arr) — среднее арифметическое.
+  const radius = getGraphAverageRadius(graph, centroid);
+
+  // Вычислим reordered = sorted(vertexes, key=azimuth).
+  const sortedVertexes = getVertexesSortedByAzimuth(graph.vertexes, centroid);
+
+  // Переберем соседние пары вершин (a = reordered[i]; b = reordered[(i + 1) % N])
+  const iter = single.limit(infinite.cycle(sortedVertexes), sortedVertexes.length+1);
+
+  let bestScore: number = -Infinity;
+  let bestAxis: LineCoefficients = [0, 0];
+
+  single.map(single.pairwise(iter), ([lhs, rhs]) => {
+    const magic = 0.5;
+
+    // Проверим ось симметрии через точки M и a
+    if (lhs.position[0] !== rhs.position[0]) {
+      const axis1 = getLineByPoints(centroid, lhs.position);
+      const score = scoreSymmetryAxis(graph, axis1, radius, magic);
+
+      if (score > bestScore) {
+        bestScore = score;
+        bestAxis = axis1;
+      }
+    }
+
+    // Проверим ось симметрии через точку M и посередине между точками a и b
+    if (lhs.position[0] !== rhs.position[0]) {
+      const axis2 = getLineByPoints(centroid, createVector(lhs.position).add(rhs.position).div(2));
+      const score = scoreSymmetryAxis(graph, axis2, radius, magic);
+
+      if (score > bestScore) {
+        bestScore = score;
+        bestAxis = axis2;
+      }
+    }
+
+    // // Проверим ось симметрии с угловым коэффициентом tan(azimuth(a)) — пытаемся провести прямую через точку a.
+    // const k1 = Math.tan(getVertexAzimuth(lhs, centroid));
+    // const p1 = lhs.position;
+    // const b1 = p1[1] - k1 * p1[0]; // y = kx + b => b = y - kx
+    // checkSymmetryAxis(graph, [k1, b1], radius, 0.5);
+    //
+    // // Проверим ось симметрии с угловым коэффициентом tan((azimuth(a) + azimuth(b)) * 0.5) — пытаемся провести
+    // // посередине между a и b (собственно, ради этого и затевалась сортировка по азимутам).
+    // const k2 = Math.tan(getVertexAzimuth(lhs, centroid) + getVertexAzimuth(rhs, centroid)) * 0.5;
+    // const p2 = createVector(lhs.position).add(rhs.position).div(2);
+    // const b2 = p2[1] - k2 * p2[0]; // y = kx + b => b = y - kx
+    // checkSymmetryAxis(graph, [k2, b2], radius, 0.5);
+  });
+
+  return [bestScore, bestAxis];
+}
