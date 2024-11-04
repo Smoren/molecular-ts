0d23b3c20eaf20053a5a88abcff9fd508bffc28f
Smoren <ofigate@gmail.com>
2024-11-04 20:03:57 +0300

    upd

--- src/scripts/lib/stdout.ts
+++ src/scripts/lib/stdout.ts
@@ -1,7 +1,11 @@
 export class StdoutInterceptor {
   private readonly originalWrite: Function;
+  private outputElement: HTMLElement | null = null;
 
   constructor() {
+    if (typeof document !== 'undefined') {
+      this.reCreateDiv();
+    }
     this.originalWrite = process.stdout.write.bind(process.stdout);
   }
 
@@ -18,9 +22,14 @@ export class StdoutInterceptor {
         const matches = chunk.match(/\./g);
         if (matches) {
           dotCount += matches.length;
-          process.stdout.clearLine(0);
-          process.stdout.cursorTo(0);
-          process.stdout.write(formatString(dotCount));
+
+          if (this.outputElement !== null) {
+            this.outputElement!.innerHTML = formatString(dotCount);
+          } else {
+            process.stdout.clearLine(0);
+            process.stdout.cursorTo(0);
+            process.stdout.write(formatString(dotCount));
+          }
           chunk = '';
         }
       }
@@ -34,5 +43,17 @@ export class StdoutInterceptor {
     process.stdout.cursorTo(0);
     // @ts-ignore
     process.stdout.write = this.originalWrite;
+
+    if (this.outputElement !== null) {
+      this.reCreateDiv();
+    }
+  }
+
+  private reCreateDiv() {
+    if (this.outputElement !== null) {
+      document.body.removeChild(this.outputElement);
+    }
+    this.outputElement = document.createElement('div');
+    document.body.appendChild(this.outputElement);
   }
 }
