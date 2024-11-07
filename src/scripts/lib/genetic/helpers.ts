9b2590c2a6ff0cbd266dd2e2d7344e0c19dc07dc
Smoren <ofigate@gmail.com>
2024-11-07 17:11:23 +0300

    upd

--- src/scripts/lib/genetic/helpers.ts
+++ src/scripts/lib/genetic/helpers.ts
@@ -2,7 +2,7 @@ import { round } from "@/lib/math";
 
 export function getScoresSummary(losses: number[], precision: number = 8): [number, number, number, number, number] {
   const best = round(losses[0], precision);
-  const second = round(losses[0], precision);
+  const second = round(losses[1], precision);
   const mean = round(losses.reduce((a, b) => a + b, 0) / losses.length, precision);
   const median = round(losses[round(losses.length/2, 0)], precision);
   const worst = round(losses[losses.length-1], precision);
