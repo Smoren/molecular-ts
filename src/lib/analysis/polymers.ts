e161c814fe5e754b732a733e4fbf0b9b9b3df377
Smoren <ofigate@gmail.com>
2024-12-31 13:19:15 +0300

    upd

--- src/lib/analysis/polymers.ts
+++ src/lib/analysis/polymers.ts
@@ -1,5 +1,5 @@
 import type { NumericVector } from "../math/types";
-import { createVector } from "../math";
+import { createVector, toVector } from "../math";
 import { convertDifferenceToNormalizedSimilarityGrade } from "./utils";
 import type { GraphInterface } from "../graph/types";
 import {
@@ -34,10 +34,10 @@ export function gradeMonomerPolymerPair(
   minPolymerSize: number = 2,
   normCoefficient: number = 0.5,
 ): PolymerSummary {
-  const monomerVertexesVector = countVertexesGroupedByType(monomerCandidate);
-  const monomerEdgesVector = countEdgesGroupedByVertexTypes(monomerCandidate);
-  const polymerVertexesVector = countVertexesGroupedByType(polymerCandidate);
-  const polymerEdgesVector = countEdgesGroupedByVertexTypes(polymerCandidate);
+  const monomerVertexesVector = toVector(countVertexesGroupedByType(monomerCandidate));
+  const monomerEdgesVector = toVector(countEdgesGroupedByVertexTypes(monomerCandidate));
+  const polymerVertexesVector = toVector(countVertexesGroupedByType(polymerCandidate));
+  const polymerEdgesVector = toVector(countEdgesGroupedByVertexTypes(polymerCandidate));
 
   const multiplier = findPolymerMultiplier(monomerVertexesVector, polymerVertexesVector);
   const polymerSize = Math.floor(multiplier);
@@ -49,20 +49,42 @@ export function gradeMonomerPolymerPair(
   const vertexesDiffVector = createVector(monomerVertexesVector).mul(multiplier).sub(polymerVertexesVector);
   const edgesDiffVector = createVector(monomerEdgesVector).mul(multiplier).sub(polymerEdgesVector);
 
-  const vertexesDiff = vertexesDiffVector.abs;
-  const edgesDiff = edgesDiffVector.abs;
+  // Extra vertexes between 2 monomers
+  const monomerVertexesDiffVector = vertexesDiffVector.clone().div(polymerSize-1);
+  // Extra edges between 2 monomers
+  const monomerEdgesDiffVector = edgesDiffVector.clone().div(polymerSize-1);
 
-  // TODO пока считаем, что полимеры соединены только одной связью, это допущение нужно исключить
-  const clarifiedEdgesDiff = edgesDiff - (polymerSize-1);
+  const monomerExtraVertexesVector = toVector(monomerVertexesDiffVector.map((x) => Math.floor(x)));
+  const monomerExtraEdgesVector = toVector(monomerEdgesVector.map((x) => Math.floor(x)));
 
-  // TODO сделать новый граф, соединив мономер с собой polymerSize раз и сравнить с полимером по полной программе
-
-  if (clarifiedEdgesDiff < 0) {
-    // не хватает связей между мономерами
+  // TODO доработать условие
+  if (monomerExtraVertexesVector.abs2 >= monomerVertexesVector.abs2 || monomerExtraEdgesVector.abs2 >= monomerEdgesVector.abs2) {
     return createBadPolymerGradeSummary(monomerCandidate.vertexes.length);
   }
 
-  const similarity = convertDifferenceToNormalizedSimilarityGrade(vertexesDiff + clarifiedEdgesDiff, normCoefficient);
+  // TODO можно попробовать выяснить, соотвтетсвуют ли дополнительные вертексы дополнительным связям
+
+  const assembledPolymerVertexesVector = monomerVertexesVector.clone().mul(polymerSize)
+    .add(monomerExtraVertexesVector.clone().mul(polymerSize-1));
+  const assembledPolymerEdgesVector = monomerEdgesVector.clone().mul(polymerSize)
+    .add(monomerExtraEdgesVector.clone().mul(polymerSize-1));
+
+  const assembledVertexesDiff = assembledPolymerVertexesVector.clone().sub(polymerVertexesVector).abs;
+  const assembledEdgesDiff = assembledPolymerEdgesVector.clone().sub(polymerEdgesVector).abs;
+
+  const similarity = convertDifferenceToNormalizedSimilarityGrade(
+    assembledVertexesDiff + assembledEdgesDiff,
+    normCoefficient,
+  );
+
+  // const monomerVertexesDiff = monomerVertexesDiffVector.abs;
+  // // TODO допущение, что дополнительная связь только одна
+  // const monomerEdgesDiff = Math.max(0, monomerEdgesDiffVector.abs-1);
+  //
+  // const similarity = convertDifferenceToNormalizedSimilarityGrade(
+  //   monomerVertexesDiff + monomerEdgesDiff,
+  //   normCoefficient,
+  // );
 
   return {
     monomerSize: monomerCandidate.vertexes.length,
