158576d4c24604e7764c9f443dbf65398aba0ed0
Smoren <ofigate@gmail.com>
2025-01-20 23:25:37 +0300

    new version of genetic lib, scheduler upd.

--- src/lib/genetic/scheduler.ts
+++ src/lib/genetic/scheduler.ts
@@ -1,6 +1,6 @@
-import type { GeneticSearchInterface, SchedulerRule } from "genetic-search";
+import type { GeneticSearchInterface, SchedulerAction } from "genetic-search";
 import type { ClusterizationWeightsConfig, SimulationGenome } from "./types";
-import { Scheduler } from "genetic-search";
+import { Scheduler, checkSchedulerCondition } from "genetic-search";
 import { single, summary } from "itertools-ts";
 import { fullCopyObject } from "../utils/functions";
 import { shuffleArray } from "../math/helpers";
@@ -49,77 +49,58 @@ export class WeightsDropout<T extends Record<string, number>> {
   }
 }
 
-export function createDropoutRule(
+export function createDropoutAction(
   weightsConfig: ClusterizationWeightsConfig,
   minDropoutCount: number,
   maxDropoutCount: number,
-): SchedulerRule<SimulationGenome, ClusterizationWeightsConfig> {
+): SchedulerAction<SimulationGenome, ClusterizationWeightsConfig> {
   const dropout = new WeightsDropout<ClusterizationWeightsConfig>(weightsConfig, minDropoutCount, maxDropoutCount);
-  return {
-    condition: () => true,
-    action: (input) => {
-      const dropped = dropout.dropout();
-      if (dropped.length) {
-        input.logger(`Weights dropout: ${dropped.join(', ')}`);
-      }
-    },
+  return (input) => {
+    const dropped = dropout.dropout();
+    if (dropped.length) {
+      input.logger(`Weights dropout: ${dropped.join(', ')}`);
+    }
   };
 }
 
-export function createMinCompoundSizeIncreaseRule(
+export function createMinCompoundSizeIncreaseAction(
   stepsInterval: number,
   maxValue?: number,
   increaseValue: number = 1,
-): SchedulerRule<SimulationGenome, ClusterizationWeightsConfig> {
-  return {
-    condition: (input) => {
-      if (maxValue !== undefined && input.config.minCompoundSize >= maxValue) {
-        return false;
-      }
-
-      return input.runner.generation % stepsInterval === 0;
-    },
-    action: (input) => {
-      input.config.minCompoundSize += increaseValue;
-      input.logger(`minCompoundSize increased (${input.config.minCompoundSize})`);
-    },
+): SchedulerAction<SimulationGenome, ClusterizationWeightsConfig> {
+  return (input) => {
+    checkSchedulerCondition(maxValue === undefined || input.config.minCompoundSize < maxValue);
+    checkSchedulerCondition(input.runner.generation % stepsInterval === 0);
+    input.config.minCompoundSize += increaseValue;
+    input.logger(`minCompoundSize increased (${input.config.minCompoundSize})`);
   };
 }
 
-export function createMinCompoundSizeDecreaseRule(
+export function createMinCompoundSizeDecreaseAction(
   decreaseDuration: number,
   minValue?: number,
   decreaseValue: number = 1,
-): SchedulerRule<SimulationGenome, ClusterizationWeightsConfig> {
-  return {
-    condition: (input) => {
-      if (minValue !== undefined && input.config.minCompoundSize <= minValue) {
-        return false;
-      }
+): SchedulerAction<SimulationGenome, ClusterizationWeightsConfig> {
+  return (input) => {
+    checkSchedulerCondition(minValue === undefined || input.config.minCompoundSize > minValue);
 
-      const historyTailLength = decreaseDuration + 1;
+    const historyTailLength = decreaseDuration + 1;
+    checkSchedulerCondition(input.history.length >= historyTailLength);
 
-      if (input.history.length < historyTailLength) {
-        return false;
-      }
+    const meanScoresHistory = input.history.map((item) => item.fitnessSummary.mean);
+    const historyTail = meanScoresHistory.slice(meanScoresHistory.length-historyTailLength);
+    checkSchedulerCondition(summary.allMatch(single.pairwise(historyTail), ([prev, next]) => prev > next));
 
-      const meanScoresHistory = input.history.map((item) => item.fitnessSummary.mean);
-
-      const historyTail = meanScoresHistory.slice(meanScoresHistory.length-historyTailLength);
-      return summary.allMatch(single.pairwise(historyTail), ([prev, next]) => prev > next);
-    },
-    action: (input) => {
-      input.config.minCompoundSize -= decreaseValue;
-      input.logger(`minCompoundSize decreased (${input.config.minCompoundSize})`);
-    },
+    input.config.minCompoundSize -= decreaseValue;
+    input.logger(`minCompoundSize decreased (${input.config.minCompoundSize})`);
   };
 }
 
-export function createDefaultClustersGradeMaximizeRules(weightsConfig: ClusterizationWeightsConfig): SchedulerRule<SimulationGenome, ClusterizationWeightsConfig>[] {
+export function createDefaultClustersGradeMaximizeActions(weightsConfig: ClusterizationWeightsConfig): SchedulerAction<SimulationGenome, ClusterizationWeightsConfig>[] {
   return [
-    createDropoutRule(weightsConfig, 0, 2),
-    // createMinCompoundSizeIncreaseRule(15, 25),
-    // createMinCompoundSizeDecreaseRule(10, 5),
+    createDropoutAction(weightsConfig, 0, 2),
+    // createMinCompoundSizeIncreaseAction(15, 25),
+    // createMinCompoundSizeDecreaseAction(10, 5),
   ];
 }
 
@@ -129,11 +110,11 @@ export function createSchedulerForClustersGradeMaximize(
   config: ClusterizationWeightsConfig,
   maxHistoryLength: number = 10,
 ): Scheduler<SimulationGenome, ClusterizationWeightsConfig> {
-  const rules = useScheduler ? createDefaultClustersGradeMaximizeRules(config) : [];
+  const actions = useScheduler ? createDefaultClustersGradeMaximizeActions(config) : [];
   return new Scheduler({
     runner,
     maxHistoryLength,
     config,
-    rules,
+    actions,
   });
 }
