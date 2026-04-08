94e997dceab19502721f9b56dacf99b409f3931d
Smoren <ofigate@gmail.com>
2026-04-08 22:43:37 +0300

    Composite genome extract tools added.

---
+++ src/lib/genetic/composite.ts
@@ -0,0 +1,41 @@
+import type { CompositeSimulationGenome, SimulationGenome } from "./types";
+import type { TypesConfig } from "../config/types";
+
+type RecursiveArray<T> = Array<T | RecursiveArray<T>>;
+
+export function extractExpressedGenome(compositeGenome: CompositeSimulationGenome): SimulationGenome {
+  return {
+    id: compositeGenome.id,
+    typesConfig: extractExpressedTypesConfig(compositeGenome.chromosomes, compositeGenome.expressionIndices),
+  }
+}
+
+export function extractExpressedTypesConfig(typesConfigs: TypesConfig[], expressionIndices: TypesConfig): TypesConfig {
+  return {
+    RADIUS: express(typesConfigs.map((x) => x.RADIUS), expressionIndices.RADIUS),
+    GRAVITY: express(typesConfigs.map((x) => x.GRAVITY), expressionIndices.GRAVITY),
+    LINK_GRAVITY: express(typesConfigs.map((x) => x.LINK_GRAVITY), expressionIndices.LINK_GRAVITY),
+    LINKS: express(typesConfigs.map((x) => x.LINKS), expressionIndices.LINKS),
+    TYPE_LINKS: express(typesConfigs.map((x) => x.TYPE_LINKS), expressionIndices.TYPE_LINKS),
+    TYPE_LINK_WEIGHTS: express(typesConfigs.map((x) => x.TYPE_LINK_WEIGHTS), expressionIndices.TYPE_LINK_WEIGHTS),
+    LINK_FACTOR_DISTANCE: express(typesConfigs.map((x) => x.LINK_FACTOR_DISTANCE), expressionIndices.LINK_FACTOR_DISTANCE),
+    LINK_FACTOR_ELASTIC: express(typesConfigs.map((x) => x.LINK_FACTOR_ELASTIC), expressionIndices.LINK_FACTOR_ELASTIC),
+    FREQUENCIES: express(typesConfigs.map((x) => x.FREQUENCIES), expressionIndices.FREQUENCIES),
+    COLORS: express(typesConfigs.map((x) => x.COLORS), expressionIndices.COLORS),
+    TRANSFORMATION: {}, // TODO implement
+  };
+}
+
+export function express<T extends RecursiveArray<number>>(chromosomes: T[], indices: T | number): T {
+  // Если индекс — это число, значит мы дошли до уровня выбора из хромосом
+  if (typeof indices === 'number') {
+    return chromosomes[indices as unknown as number];
+  }
+
+  // Если это массив (маска), идем глубже
+  return indices.map((subIndices: any, i: number) => {
+    // Пробрасываем массив хромосом, но выбираем из каждой i-й элемент
+    const subChromosomes = chromosomes.map(c => (c as any)[i]);
+    return express(subChromosomes, subIndices);
+  }) as unknown as T;
+}
